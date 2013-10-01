67d0e81bb4781a7c34e132b81d9c123714333739
骆昱 <indie.luo@gmail.com>
2013-10-01 17:11:20 +0800

    ADD: chat component

---
+++ LYTool/_Chat/LYChat.h
@@ -0,0 +1,13 @@
+//
+//  LYChat.h
+//  LYToolSample
+//
+//  Created by Rick Luo on 2013-10-01.
+//  Copyright (c) 2013 Luo Yu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LYChat : NSObject
+
+@end
---
+++ LYTool/_Chat/LYChat.m
@@ -0,0 +1,13 @@
+//
+//  LYChat.m
+//  LYToolSample
+//
+//  Created by Rick Luo on 2013-10-01.
+//  Copyright (c) 2013 Luo Yu. All rights reserved.
+//
+
+#import "LYChat.h"
+
+@implementation LYChat
+
+@end
