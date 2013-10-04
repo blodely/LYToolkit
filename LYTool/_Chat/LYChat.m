53a733248fb53e0f775427e330970cff8a8aa369
骆昱 <indie.luo@gmail.com>
2013-10-04 17:50:57 +0800

    MODIFY: nscoding

--- LYTool/_Chat/LYChat.m
+++ LYTool/_Chat/LYChat.m
@@ -12,4 +12,18 @@
 
 #pragma mark - NSCODING
 
+- (id)initWithCoder:(NSCoder *)aDecoder {
+	if (self = [super init]) {
+		_message = [aDecoder decodeObjectForKey:@"ly.chat.message"];
+		_sentDate = [aDecoder decodeObjectForKey:@"ly.chat.sent.date"];
+	}
+	return self;
+}
+
+- (void)encodeWithCoder:(NSCoder *)aCoder {
+	
+	[aCoder encodeObject:self.message forKey:@"ly.chat.message"];
+	[aCoder encodeObject:self.sentDate forKey:@"ly.chat.sent.date"];
+}
+
 @end
