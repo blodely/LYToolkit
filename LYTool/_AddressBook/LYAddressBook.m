1bab926442d67da2634c0ed2078d8f98de746c8e
骆昱 <indie.luo@gmail.com>
2013-10-09 17:28:57 +0800

    MODIFY: +contactsAfter: implementation

--- LYTool/_AddressBook/LYAddressBook.m
+++ LYTool/_AddressBook/LYAddressBook.m
@@ -90,7 +90,58 @@
 #pragma mark -
 
 + (NSArray *)contactsAfter:(NSDate *)after {
-	return nil;
+	
+	NSMutableArray *contacts = [NSMutableArray arrayWithCapacity:1];
+	double intervalAfter = [after timeIntervalSince1970];
+	
+	CFErrorRef *error = nil;
+	ABAddressBookRef addressbook = ABAddressBookCreateWithOptions(NULL, error);
+	
+	// READ ALL
+	NSArray *allContacts = (__bridge_transfer NSArray *)ABAddressBookCopyArrayOfAllPeople(addressbook);
+	
+	CFRelease(addressbook);
+	
+	for (int i = 0; i < [allContacts count]; i++) {
+		
+		ABRecordRef one = (__bridge ABRecordRef)allContacts[i];
+		
+		if ([(__bridge_transfer NSDate *)ABRecordCopyValue(one, kABPersonModificationDateProperty) timeIntervalSince1970] > intervalAfter) {
+			// MODIFY DATE
+			
+			ABMultiValueRef numbers = ABRecordCopyValue(one, kABPersonPhoneProperty);
+			
+			// SPLIT BY NUMBER
+			for (int j = 0; j < ABMultiValueGetCount(numbers); j++) {
+				
+				LYContact *aContact = [[LYContact alloc] init];
+				
+				NSString *firstName = (__bridge_transfer NSString *)ABRecordCopyValue(one, kABPersonFirstNameProperty);
+				NSString *lastName = (__bridge_transfer NSString *)ABRecordCopyValue(one, kABPersonLastNameProperty);
+				
+				aContact.name = !firstName && !lastName ? [NSString stringWithFormat:@"%@ %@", firstName, lastName] : (!firstName ? firstName : (!lastName ? lastName : @" " ));
+				if (aContact.name != nil && ![aContact.name isEqualToString:@""] && ![aContact.name isEqualToString:@" "]) {
+					aContact.sort = [[NSString stringWithFormat:@"%c", pinyinFirstLetter([aContact.name characterAtIndex:0])] uppercaseString];
+				} else {
+					aContact.sort = @"#";
+				}
+				aContact.number = [(__bridge_transfer NSString *)ABMultiValueCopyValueAtIndex(numbers, j) phoneNumber];
+				aContact.lastSave = (__bridge_transfer NSDate *)ABRecordCopyValue(one, kABPersonModificationDateProperty);
+				
+				[contacts addObject:aContact];
+				
+				aContact = nil;
+			}
+			
+		}
+		
+		CFRelease(one);
+	}
+	
+	// SORT RECORD
+	[contacts sortedArrayUsingDescriptors:@[[NSSortDescriptor sortDescriptorWithKey:@"name" ascending:YES selector:@selector(caseInsensitiveCompare:)], ]];
+	
+	return [NSArray arrayWithArray:contacts];
 }
 
 @end
