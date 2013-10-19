2fe68e0249d3b80d5d29c03f3a501ec4fd6710d6
骆昱 <indie.luo@gmail.com>
2013-10-19 17:18:46 +0800

    ADD: contact table view

---
+++ LYToolSample/LYToolSample/_Scene/_Contact/ContactViewController.h
@@ -0,0 +1,13 @@
+//
+//  ContactViewController.h
+//  LYToolSample
+//
+//  Created by Rick Luo on 10/19/13.
+//  Copyright (c) 2013 Luo Yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContactViewController : UIViewController
+
+@end
